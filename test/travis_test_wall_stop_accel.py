f4ede4c0d2f415aaf2bd00e21842da56de7e79b6
takata okada <aozoratakata@gmail.com>
2020-01-20 15:27:06 +0900

    Add files

--- test/travis_test_wall_stop_accel.py
+++ test/travis_test_wall_stop_accel.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 import unittest, rostest
 import rosnode, rospy
-import time,sys
+import time
 
-class WallStopTest(unittest.TestCase):
+class WallStopAccelTest(unittest.TestCase):
     def set_and_get(self,lf,ls,rs,rf):
         with open("/dev/rtlightsensor0","w") as f:
             f.write("%d %d %d %d\n" % (rf,rs,ls,lf))
@@ -15,7 +15,6 @@ class WallStopTest(unittest.TestCase):
             left = int(lf.readline().rstrip())
             right = int(rf.readline().rstrip())
 
-        print >> sys.stderr, "left:%d right:%d" % (left,right)
         return left, right
 
     def test_io(self):
@@ -30,9 +29,9 @@ class WallStopTest(unittest.TestCase):
         self.assertTrue(2000 < left == right, "can't accerelate")
 
         left, right = self.set_and_get(15,0,20,15) #total: 50
-        self.assertTrue(left == right == 0, "can't stop")
+        self.assertTrue(left == right == 0, "can't stop again")
 
 if __name__ == '__main__':
     time.sleep(3)
     rospy.init_node('travis_test_wall_stop_accel')
-    rostest.rosrun('pimouse_run_corridor','travis_test_wall_stop_accel',WallStopTest)
+    rostest.rosrun('pimouse_run_corridor','travis_test_wall_stop_accel',WallStopAccelTest)
