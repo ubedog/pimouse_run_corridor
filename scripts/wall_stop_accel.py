6d5591e91557553e5ecf0c5bdf1424dae490edf6
takata okada <aozoratakata@gmail.com>
2021-02-02 11:07:43 +0900

    コミットした

--- scripts/wall_stop_accel.py
+++ scripts/wall_stop_accel.py
@@ -5,23 +5,21 @@ from geometry_msgs.msg import Twist
 from std_srvs.srv import Trigger, TriggerResponse
 from pimouse_ros.msg import LightSensorValues
 
-class WallStop():
+class WallTrace():
     def __init__(self):
         self.cmd_vel = rospy.Publisher('/cmd_vel',Twist,queue_size=1)
+
+        self.sensor_values = LightSensorValues()
         rospy.Subscriber('/lightsensors', LightSensorValues, self.callback_sensors)
-        self.sensors = LightSensorValues()
+        
 
-    def callback_sensors(self,messages):
-        self.sensors = messages
+    def callback_lightsensors(self,messages):
+        self.sensors_values = messages
 
-    def decision(self,sensor_sum_all):
-        linear = 0.0 if sensor_sum_all >= 500 else 0.5
-        angular = 0.0
-        return linear, angular
 
     def run(self):
         rate = rospy.Rate(10)
-        d = Twist()
+        data = Twist()
 
         accel = 0.02
         data.linear.x = 0.0
@@ -44,12 +42,11 @@ class WallStop():
 
 if __name__ == '__main__':
     rospy.init_node('wall_trace')
+
     rospy.wait_for_service('/motor_on')
     rospy.wait_for_service('/motor_off')
-
     rospy.on_shutdown(rospy.ServiceProxy('/motor_off',Trigger).call)
-    if not rospy.ServiceProxy('/motor_on',Trigger).call().success:
-        rospy.logerr("motors are not empowered")
-        sys.exit(1)
-
-    WallStop().run()
+    rospy.ServiceProxy('/motor_on',Trigger).call()
+    
+    w = WallTrace()
+    w.run()
